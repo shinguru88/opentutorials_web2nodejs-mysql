99895990ae894f3af36b7cc3958a552e23e61063
Shin, Hyun-Chool <shinguru@gmail.com>
2019-01-01 19:04:03 +0900

    change modify function by using mysql

--- main.js
+++ main.js
@@ -61,6 +61,9 @@ var app = http.createServer(function (request, response) {
     }
   } else if (pathname === '/create') {
     db.query('SELECT * from topic', function (error, topics) {
+      if (error) {
+        throw error;
+      }
       var title = 'WEB - create';
       var list = template.list(topics);
       var html = template.HTML(title, list, `
@@ -97,15 +100,21 @@ var app = http.createServer(function (request, response) {
         });
     });
   } else if (pathname === '/update') {
-    fs.readdir('./data', function (error, filelist) {
-      var filteredId = path.parse(queryData.id).base;
-      fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-        var title = queryData.id;
-        var list = template.list(filelist);
+    db.query('SELECT * from topic', function (error, topics) {
+      if (error) {
+        throw error;
+      }
+      db.query('SELECT * from topic WHERE id=?', [queryData.id], function (error2, topic) {
+        if (error2) {
+          throw error2;
+        }
+        var title = topic[0].title;
+        var description = topic[0].description;
+        var list = template.list(topics);
         var html = template.HTML(title, list,
           `
             <form action="/update_process" method="post">
-              <input type="hidden" name="id" value="${title}">
+              <input type="hidden" name="id" value="${topic[0].id}">
               <p><input type="text" name="title" placeholder="title" value="${title}"></p>
               <p>
                 <textarea name="description" placeholder="description">${description}</textarea>
@@ -115,7 +124,7 @@ var app = http.createServer(function (request, response) {
               </p>
             </form>
             `,
-          `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+          `<a href="/create">create</a> <a href="/update?id=${queryData.id}">update</a>`
         );
         response.writeHead(200);
         response.end(html);
@@ -131,11 +140,13 @@ var app = http.createServer(function (request, response) {
       var id = post.id;
       var title = post.title;
       var description = post.description;
-      fs.rename(`data/${id}`, `data/${title}`, function (error) {
-        fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-          response.writeHead(302, { Location: `/?id=${title}` });
-          response.end();
-        })
+
+      db.query(`UPDATE topic SET title=?, description=? WHERE id=?;`, [title, description, id], function(error, result) {
+        if (error) {
+          throw error;
+        }
+        response.writeHead(302, { Location: `/?id=${id}` });
+        response.end();
       });
     });
   } else if (pathname === '/delete_process') {
@@ -146,11 +157,15 @@ var app = http.createServer(function (request, response) {
     request.on('end', function () {
       var post = qs.parse(body);
       var id = post.id;
-      var filteredId = path.parse(id).base;
-      fs.unlink(`data/${filteredId}`, function (error) {
+
+      db.query(`DELETE FROM topic WHERE id=?;`, [id], function(error, result) {
+        if (error) {
+          throw error;
+        }
+
         response.writeHead(302, { Location: `/` });
         response.end();
-      })
+      });
     });
   } else {
     response.writeHead(404);
